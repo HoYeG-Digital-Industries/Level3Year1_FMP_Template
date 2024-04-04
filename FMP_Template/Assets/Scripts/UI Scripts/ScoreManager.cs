928a9f2ee9b1fdcfa88627dce787eac65b7b3f97
Rob Charlish <rob.charlish@gmail.com>
2024-04-04 12:14:37 +0100

    Added in new Main Scene and probuilder env.
    Its in there

--- FMP_Template/Assets/Scripts/UI Scripts/ScoreManager.cs
+++ FMP_Template/Assets/Scripts/UI Scripts/ScoreManager.cs
@@ -7,26 +7,26 @@ using UnityEditor.SearchService;
 public class ScoreManager : MonoBehaviour
 {
 
-    public TMP_Text ScoreContainer;
-    private int baseScore;
+	public TMP_Text ScoreContainer;
+	private int baseScore;
 
 
-    public void AddScore(int score) //Call this in order to add score 
-    {
-        baseScore += score;
-        UpdateScore();
-    }
+	public void AddScore(int score) //Call this in order to add score 
+	{
+		baseScore += score;
+		UpdateScore();
+	}
 
-    public void SubtractScore(int score) //Call this in order to subtract score
-    {
-        baseScore -= score;
-        UpdateScore();
-    }
+	public void SubtractScore(int score) //Call this in order to subtract score
+	{
+		baseScore -= score;
+		UpdateScore();
+	}
 
-    void UpdateScore() //Call this is order to update the UI
-    {
-        ScoreContainer.text = "SCORE: " + baseScore;
-    }
-
+	void UpdateScore() //Call this is order to update the UI
+	{
+		ScoreContainer.text = "SCORE: " + baseScore;
+	}
+	
 
 }
